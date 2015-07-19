e645175b379fe65c89c2c30922650c7baa1c5cbf
Floris Luiten <ubuntu@lenwweb.nl>
2015-07-19 09:53:44 +0200

    Introduced unit test

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Please feel free to help making this app better by opening an issue or create a 
 
 # Usage
 The RSS Reader is designed to work with Ubuntu Touch and should be installed through Ubuntu Store.
+
+# Testing
+The tests are written for jasmine in combination with grunt. Run `npm install` once to install the dependencies. Then run tests with `npm test`. All tests should be checked before each commit to ensure every commit results in a runable app.
--- tests/unit/specs/app-specs.js
+++ tests/unit/specs/app-specs.js
@@ -1,16 +1,19 @@
-describe("App", function() {
+describe("RSSreader", function() {
     var application;
 
     var MockedUIContext = function() {}
-    MockedUIContext.prototype.init = function() { }
+    MockedUIContext.prototype.init = function() {
+        this.pagestack = [];
+    };
 
     beforeEach(function() {
-        application = new Application(MockedUIContext);
+        application = new Rssreader(MockedUIContext);
     });
 
-    it("should have a proper initialized flag", function() {
+    it("should have the debug flag disabled", function() {
         application.init();
-        expect(application.initialized()).toBeTruthy();
+
+        expect(application.settings.debug).toBe(false);
     });
 });
 
